8ac1c3987bfdc8f9832712f0a92824fed4043b30
muhammadmohsin123 <mohsinsurani12@gmail.com>
2022-09-07 21:15:25 +0500

    dequeue added

---
+++ Stacks and Queues/Queue.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first();
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+  }
+  dequeue() {
+    if (this.length === 0) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+  }
+}
+
+const myQueue = new Queue();
+
+myQueue.enqueue(5);
+myQueue.enqueue(10);
+myQueue.enqueue(15);
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+console.log(myQueue);
